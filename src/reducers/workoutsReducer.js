6ef4e0bbba0e0fd6529716f3fb8691223c20d47e
Yoshi <yoshimejia00@gmail.com>
2021-11-13 11:41:45 -0500

    began building reducer

---
+++ src/reducers/workoutsReducer.js
@@ -0,0 +1,15 @@
+const workoutsReducer = (state = { workouts: []}, action) => {
+    debugger
+    switch (action.type) {
+      case "FETCH_WORKOUTS":
+        return {
+          ...state,
+          workouts: action.workouts,
+        };
+      default:
+        return state;
+    }
+  };
+
+  export default workoutsReducer
+  
