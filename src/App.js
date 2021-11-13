39d848661fdc4bd58b6443005af4b513c9219ea8
Yoshi <yoshimejia00@gmail.com>
2021-11-13 11:40:47 -0500

    rendering workouts container to begin testing fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import WorkoutsContainer from './containers/WorkoutsContainer';
+
 
 function App() {
   return (
@@ -7,16 +9,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+        <WorkoutsContainer />
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
