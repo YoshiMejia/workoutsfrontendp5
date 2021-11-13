93649854f5192ff0b268c4d32bd503a1cf93127a
Yoshi <yoshimejia00@gmail.com>
2021-11-13 13:26:48 -0500

    fixed page styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import WorkoutsContainer from './containers/WorkoutsContainer';
 
@@ -7,11 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <WorkoutsContainer />
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
 
       </header>
     </div>
