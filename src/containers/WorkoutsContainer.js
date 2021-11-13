94a05cdbcc242d9dd12bdf4082bbae63ecbc3f07
Yoshi <yoshimejia00@gmail.com>
2021-11-13 12:25:27 -0500

    added functions for fetching API data and creating divs for each object

--- src/containers/WorkoutsContainer.js
+++ src/containers/WorkoutsContainer.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import WorkoutCard from '../components/WorkoutCard'
 
 export default class WorkoutsContainer extends React.Component {
     state = {
         workouts: []
     }
+
+    createWorkoutCard(){
+        return this.state.workouts.map((workout) => <WorkoutCard workout={workout} /> )
+    }
+
+    componentDidMount(){
+        const url ="http://127.0.0.1:3000/workouts"
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            // debugger
+            
+           this.setState({
+               workouts: json
+           })
+        })
+      }
+
     render(){
        return (
        <div id="workouts-container">
            {/* workoutform component will go here */}
             <h1>Workouts container being rendered</h1>
+            {this.createWorkoutCard()}
         </div>
         )
     }
