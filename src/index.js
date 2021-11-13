71bcb9d331bfcb319ecff47ae54e8be94d0059ce
Yoshi <yoshimejia00@gmail.com>
2021-11-13 11:40:57 -0500

    imported dependencies and created store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import thunk from "redux-thunk";
+import workoutsReducer from './reducers/workoutsReducer'
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
+
+
+const store = createStore(workoutsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+   <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
