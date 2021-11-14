8c20b029405602305665475e8571994eb724d88c
Yoshi <yoshimejia00@gmail.com>
2021-11-14 13:22:34 -0500

    implementing react-router for navbar

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import React from 'react';
 import './App.css';
 import WorkoutsContainer from './containers/WorkoutsContainer';
 import PlannersContainer from './containers/PlannersContainer';
+import {
+  BrowserRouter, Routes,
+  Route
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
 
 export default class App extends React.Component {
 
   render(){
     return (
-    <div className="App">
+      <div className="App">
       <header className="App-header">
+        <Navbar store={this.props.store}/>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <PlannersContainer />
-        <WorkoutsContainer store={this.props.store}/>
-      </header>
-    </div>
-  );
-}
+        {/* <BrowserRouter>
+          <Routes>
+        {/* <div className="navbar"> 
+          <Route path="/" element={<Home />} />
+          <Route path="planners" element={<PlannersContainer />} />
+          <Route path="workouts" element={<WorkoutsContainer store={this.props.store}/>} />
+         </div>
+       </Routes>
+      </BrowserRouter>  */}
+
+      {/* <WorkoutsContainer store={this.props.store}/> */}
+       
+       </header>
+         {/* <PlannersContainer /> */}
+     </div>
+   );
+ }
 
 }
 
