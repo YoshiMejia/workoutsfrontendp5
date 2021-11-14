93d20e875423adc92fb834638f958d3e4a59cdb7
Yoshi <yoshimejia00@gmail.com>
2021-11-14 09:27:58 -0500

    working on reducer to handle more actions

--- src/reducers/workoutsReducer.js
+++ src/reducers/workoutsReducer.js
@@ -2,11 +2,14 @@ const workoutsReducer = (state = { workouts: []}, action) => {
     // debugger
     console.log('inside reducer');
     switch (action.type) {
-      case "NEW_WORKOUT":
+      case "ADD_WORKOUT":
+        console.log({ workouts: [...state.workouts, action.payload] })
         return {
-          ...state,
-          workouts: action.workouts,
-        };
+        //   ...state,
+        //   workouts: action.workouts,
+        // }, () => console.log(state)
+          workouts: [...state.workouts, action.payload]
+        }
       default:
         return state;
     }
