f68ec59c0d1452bf4f044a215c239788117fd583
Yoshi <yoshimejia00@gmail.com>
2021-11-14 09:25:53 -0500

    rendering planners container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import WorkoutsContainer from './containers/WorkoutsContainer';
+import PlannersContainer from './containers/PlannersContainer';
 
+export default class App extends React.Component {
 
-function App() {
-  return (
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <WorkoutsContainer />
-
+        <PlannersContainer />
+        <WorkoutsContainer store={this.props.store}/>
       </header>
     </div>
   );
 }
 
-export default App;
+}
+
