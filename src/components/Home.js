495e3ce2e9b3edd14f5d3ff55d6145a46fa680b0
Yoshi <yoshimejia00@gmail.com>
2021-11-14 16:32:09 -0500

    created small bio for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 export function Home() {
     return (
       <div style={{ padding: 20 }}>
-        <h2>Home View</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+        <h2>Workout Planner</h2>
+        <p>Welcome to Workout Planner!</p>
+        <p>Click on one of the links above to start.</p>
       </div>
     );
   }
