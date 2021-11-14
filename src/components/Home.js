1b94dfb8cd26f7e0b180e0a1133acdcdb081fc45
Yoshi <yoshimejia00@gmail.com>
2021-11-14 13:23:41 -0500

    created (temp) Home component for testing router

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+export function Home() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h2>Home View</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+      </div>
+    );
+  }
