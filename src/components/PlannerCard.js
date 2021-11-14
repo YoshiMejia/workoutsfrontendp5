e601cb4667a99fc6664a1ea0263bc5b33970c14b
Yoshi <yoshimejia00@gmail.com>
2021-11-14 13:24:17 -0500

    switched to class component

--- src/components/PlannerCard.js
+++ src/components/PlannerCard.js
@@ -1,13 +1,28 @@
-function PlannerCard({planner, workouts}){
-    // debugger
-    return(
+import React from 'react'
+import { connect } from 'react-redux'
+
+class PlannerCard extends React.Component{
+    
+    render(){ 
+        const workouts = (
+            this.props.workouts.map((workout) =>  
+            <p>{workout.name}</p> )
+        )
+
+        return (
         <div className="plannerCard">
-             <div id={planner.id} key={planner.id}>
-                <h2>Name: {planner.name}</h2>
-                <ul>Workouts: {workouts}</ul>
+       <div id={this.props.planner.id} key={this.props.planner.id}>
+            {console.log("inside planner card", workouts)}
+                 <h2>Name: {this.props.planner.name}</h2>
+                 <h4>Workouts: {workouts}</h4>
             </div>
-        </div>
-    )
+         </div>
+    )}
 }
 
-export default PlannerCard
+const mapState = state => {
+    // debugger
+    return { workouts: state.workouts}
+}
+
+export default connect(mapState)(PlannerCard)
