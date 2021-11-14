53cb4b467f3fafbe90801af61e83e96c6ced9c48
Yoshi <yoshimejia00@gmail.com>
2021-11-14 15:20:12 -0500

    added action for completing workout to reducer

--- src/reducers/workoutsReducer.js
+++ src/reducers/workoutsReducer.js
@@ -1,4 +1,5 @@
 const workoutsReducer = (state = { workouts: []}, action) => {
+  // debugger
     switch (action.type) {
       case "NEW_WORKOUT":
         console.log("new workout action", { workouts: [...state.workouts.concat(action.workout)] })
@@ -9,8 +10,15 @@ const workoutsReducer = (state = { workouts: []}, action) => {
           const workouts = action.workout.map((workout) => workout)
           console.log("fetch workouts action", ...state.workouts.concat(workouts))
           return{
-            workouts: [...state.workouts.concat(workouts)]
+            // workouts: [...state.workouts.concat(workouts)]
+            workouts: [...state.workouts.filter((workout) => workout.id === action.id)]
           }
+          case "COMPLETE":
+            console.log("completing action",{workouts: [...state.workouts], completed: [action.workout]})
+            return{
+              workouts: [...state.workouts], 
+              completed: [action]
+            }
       default:
         return state;
     }
