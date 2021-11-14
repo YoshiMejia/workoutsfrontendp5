c760631fceb3a715e2b3efd2b375c597d262b266
Yoshi <yoshimejia00@gmail.com>
2021-11-14 15:19:12 -0500

    working on completion button functionality

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -1,16 +1,24 @@
-function WorkoutCard({workout}){
-    // debugger
-    return(
+import { completeWorkout } from "../actions/actions"
+import { connect } from 'react-redux'
+import React from 'react'
+
+
+class WorkoutCard extends React.Component{
+    render(){
+        // debugger
+        const workout = this.props.workout
+        return(
         <div className="WorkoutCard">
             <div id={workout.id} key={workout.id}>
                 <h2>Name: {workout.name}</h2>
                 <h4>Description: {workout.description}</h4>
                 <p>Reps: {workout.reps}</p>
                 <p>Sets: {workout.sets}</p>
-                <button>Completed workout</button>
+                <button onClick={() => this.props.completeWorkout(workout)} >Completed workout</button>
             </div>
         </div>
-    )
+    )}
 }
+//check if complete button works 
 
-export default WorkoutCard
+export default connect(null, {completeWorkout})(WorkoutCard)
