21bdff109753634322d8e0e5f9de989a8ad3d77b
Yoshi <yoshimejia00@gmail.com>
2021-11-15 13:47:11 -0500

    removed personal comments

--- src/containers/PlannersContainer.js
+++ src/containers/PlannersContainer.js
@@ -9,7 +9,6 @@ export default class PlannersContainer extends React.Component {
     }
 
     createPlannerCard(){
-        // return this.state.planners.map((planner) => <PlannerCard planner={planner} workouts={this.state.workouts.map((workout) => <li key={workout.id}>{workout.name}</li>)} />)
         return this.state.planners.map((planner) => <PlannerCard planner={planner} />)
     }
 
@@ -22,7 +21,7 @@ export default class PlannersContainer extends React.Component {
         })
         this.createPlannerCard()
     }
-//switch this component & workout component to fetch using actions
+  
     componentDidMount(){
         const url ="http://127.0.0.1:3000/planners"
         fetch(url)
@@ -30,7 +29,6 @@ export default class PlannersContainer extends React.Component {
         .then(json => {
             const planner = json.map((planner) => planner)
             const workouts = json[0].workouts.map((workout) => workout)
-            // debugger
            this.setState({
                planners: planner,
                workouts: workouts
