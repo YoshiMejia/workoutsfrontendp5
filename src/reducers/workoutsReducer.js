3b957a6238d2c76d0c0da38691b7def6776dd49e
Yoshi <yoshimejia00@gmail.com>
2021-11-15 13:47:34 -0500

    removed comments and unused action

--- src/reducers/workoutsReducer.js
+++ src/reducers/workoutsReducer.js
@@ -1,18 +1,10 @@
 const workoutsReducer = (state = { workouts: []}, action) => {
-  // debugger
     switch (action.type) {
       case "NEW_WORKOUT":
         console.log("new workout action", { workouts: [...state.workouts.concat(action.workout)] })
         return {
           workouts: [...state.workouts.concat(action.workout)]
         }
-        case "FETCH_WORKOUTS":
-          const workouts = action.workout.map((workout) => workout)
-          console.log("fetch workouts action", ...state.workouts.concat(workouts))
-          return{
-            // workouts: [...state.workouts.concat(workouts)]
-            workouts: [...state.workouts.filter((workout) => workout.id === action.id)]
-          }
           case "COMPLETE":
             console.log("checking new action",{ workouts: [...state.workouts.concat(action.workout)] })
             return{
