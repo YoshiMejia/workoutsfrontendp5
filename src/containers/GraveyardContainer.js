5233d719a67eb10db4a2b2ddcc4cba9d050bc44e
Yoshi <yoshimejia00@gmail.com>
2021-11-15 13:47:03 -0500

    removed debuggers

--- src/containers/GraveyardContainer.js
+++ src/containers/GraveyardContainer.js
@@ -4,19 +4,16 @@ import { connect } from 'react-redux'
 
 class GraveyardContainer extends React.Component {
     showCompleted = () => {
-        // debugger
         if(this.props.workouts.length !== 0){
             const workouts = this.props.workouts.map((w) => w.name)
             const listWorkouts = workouts.map((w, index) => <li key={index}>{w}</li>)
-            // debugger
             return listWorkouts
         } else {
             return "Completed workouts will go here"
         }
     }
 
-    render(){ 
-        // debugger        
+    render(){      
         return(
             <div className="graveyard">
                 <h1>
