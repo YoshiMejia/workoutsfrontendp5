675d9735237a3180a782ead3f157f52916679848
Yoshi <yoshimejia00@gmail.com>
2021-11-16 15:15:29 -0500

    erased comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-// import WorkoutsContainer from './containers/WorkoutsContainer';
-// import PlannersContainer from './containers/PlannersContainer';
-// import {
-//   BrowserRouter, Routes,
-//   Route
-// } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import { Home } from './components/Home';
 
 export default class App extends React.Component {
 
@@ -17,21 +9,7 @@ export default class App extends React.Component {
       <div className="App">
       <header className="App-header">
         <Navbar store={this.props.store}/>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <BrowserRouter>
-          <Routes>
-        {/* <div className="navbar"> 
-          <Route path="/" element={<Home />} />
-          <Route path="planners" element={<PlannersContainer />} />
-          <Route path="workouts" element={<WorkoutsContainer store={this.props.store}/>} />
-         </div>
-       </Routes>
-      </BrowserRouter>  */}
-
-      {/* <WorkoutsContainer store={this.props.store}/> */}
-       
        </header>
-         {/* <PlannersContainer /> */}
      </div>
    );
  }
