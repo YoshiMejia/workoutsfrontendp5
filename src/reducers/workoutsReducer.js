6d33b6a0f1d9c62a440eb61344804f519eef790a
Yoshi <yoshimejia00@gmail.com>
2021-11-18 08:50:02 -0500

    added fetch action to reducer to fetch data

--- src/reducers/workoutsReducer.js
+++ src/reducers/workoutsReducer.js
@@ -1,15 +1,27 @@
-const workoutsReducer = (state = { workouts: []}, action) => {
+const workoutsReducer = (state = { workouts: [], completed: [] }, action) => {
     switch (action.type) {
       case "NEW_WORKOUT":
-        console.log("new workout action", { workouts: [...state.workouts.concat(action.workout)] })
+        console.log("new workout action", { workouts: [...state.workouts.concat(action.workout)] , 
+          completed: [...state.completed]})
         return {
-          workouts: [...state.workouts.concat(action.workout)]
+          workouts: [...state.workouts.concat(action.workout)], 
+          completed: [...state.completed]
         }
-          case "COMPLETE":
-            console.log("checking new action",{ workouts: [...state.workouts.concat(action.workout)] })
-            return{
-              workouts: [...state.workouts.concat(action.workout)]
-            }
+      case "COMPLETE":
+        console.log("checking new action",{ workouts: [...state.workouts], 
+          completed: [...state.completed.concat(action.workout)]})
+        // debugger
+        return{
+          workouts: [...state.workouts], 
+          completed: [...state.completed.concat(action.workout)]
+        }
+        case "FETCH_WORKOUTS":
+        console.log('fetching data', { workouts: [...state.workouts.concat(action.workout)] , 
+          completed: [...state.completed]});
+          return{
+            workouts: [...state.workouts.concat(action.workout)], 
+          completed: [...state.completed]
+          }
       default:
         return state;
     }
