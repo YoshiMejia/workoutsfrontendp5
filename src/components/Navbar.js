41f515d8ec4693a775c96ba6cfc3bece38835cec
Yoshi <yoshimejia00@gmail.com>
2022-03-16 14:53:45 -0400

    using bootstrap for navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,49 @@
-// import React from 'react';
-// import { Home } from './Home';
-// import WorkoutsContainer from '../containers/WorkoutsContainer';
-// import PlannersContainer from '../containers/PlannersContainer';
-// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-// import GraveyardContainer from '../containers/GraveyardContainer';
-
-// const Navbar = () => {
-//   return (
-//     <div className="navbar">
-//         <BrowserRouter>
-//         <div className="links">
-//             <nav>
-//                 <Link to="/">Home</Link>
-//             <br />
-//                 <Link to="/planners">Planners</Link>
-//             <br />
-//                 <Link to="/workouts">Workouts</Link>
-//             <br />
-//                 <Link to="/graveyard">Completed Workouts</Link>
-//             </nav>
-//             </div>
-//         <div className ="routes">
-//             <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="planners" element={<PlannersContainer />} />
-//                 <Route path="workouts" element={<WorkoutsContainer />} />
-//                 <Route path="graveyard" element={<GraveyardContainer />} />
-//             </Routes>
-//         </div>
-//         </BrowserRouter>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
 
 import React from 'react';
 import { Home } from './Home';
 import WorkoutsContainer from '../containers/WorkoutsContainer';
 import PlannersContainer from '../containers/PlannersContainer';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import GraveyardContainer from '../containers/GraveyardContainer';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { LinkContainer } from 'react-router-bootstrap';
+
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <BrowserRouter>
-        <div className="links">
-            <nav>
-                <Link to="/">Home</Link>
-            <br />
-                <Link to="/planners">Planners</Link>
-            <br />
-                <Link to="/workouts">Workouts</Link>
-            <br />
-                <Link to="/graveyard">Completed Workouts</Link>
-            </nav>
-            </div>
-        <div className ="routes">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="planners" element={<PlannersContainer />} />
-                <Route path="workouts" element={<WorkoutsContainer />} />
-                <Route path="graveyard" element={<GraveyardContainer />} />
-            </Routes>
+    return(
+        <div className='navbar'>
+            <MemoryRouter>
+                <Container className='p-3'>
+                    <Container className='p-5 mb-4 bg-light rounded-3'>
+                        {/* Routes == Switch */}
+                        <Routes>
+                            <Route path='/' element={<Home/>} />
+                            <Route path='/planners' element={<PlannersContainer/>} />
+                            <Route path='/workouts' element={<WorkoutsContainer/>} />
+                            <Route path='/graveyard' element={<GraveyardContainer/>} />
+                        </Routes>
+                        {/* Routes == Switch */}
+                        <ButtonToolbar className='custom-btn-toolbar fixed-top'>
+                            <LinkContainer to='/'>
+                                <Button>Home</Button>
+                            </LinkContainer>
+                            <LinkContainer to='/planners'>
+                                <Button>Planners</Button>
+                            </LinkContainer>
+                            <LinkContainer to='/workouts'>
+                                <Button>Workouts</Button>
+                            </LinkContainer>
+                            <LinkContainer to='/graveyard'>
+                                <Button>Completed Workouts</Button>
+                            </LinkContainer>
+                        </ButtonToolbar>
+                    </Container>
+                </Container>
+            </MemoryRouter>
         </div>
-        </BrowserRouter>
-    </div>
-  );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
