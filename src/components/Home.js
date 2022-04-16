1db02a7316d775bdbc019e42a6f28c727d3a093e
Yoshi <yoshimejia00@gmail.com>
2022-04-16 17:52:05 -0400

    removed redundant header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 export function Home() {
     return (
       <div style={{ padding: 20 }}>
-        <h2>Workout Planner</h2>
         <p>Welcome to Workout Planner!</p>
         <p>Click on one of the links above to start.</p>
       </div>
