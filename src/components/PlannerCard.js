1f2954064d6ffeadba9cd8c3c04e7a61fbb9c0b1
Yoshi <yoshimejia00@gmail.com>
2022-05-03 21:01:33 -0400

    clean-up: erased console.log

--- src/components/PlannerCard.js
+++ src/components/PlannerCard.js
@@ -7,17 +7,18 @@ class PlannerCard extends React.Component{
         const workouts = (
             this.props.planner.workouts.map((workout) => workout.name )
         )
+
         const listWorkouts = workouts.map((w, index) => <li key={index}>{w}</li>)
         
         return (
-        <div className="plannerCard">
-       <div id={this.props.planner.id} key={this.props.planner.id}>
-            {console.log("inside planner card", workouts)}
-                 <h2>Planner name: {this.props.planner.name}</h2>
-                 <ul>Workouts in this planner: {listWorkouts}</ul>
+            <div className="plannerCard">
+                <div id={this.props.planner.id} key={this.props.planner.id}>
+                    <h2>Planner name: {this.props.planner.name}</h2>
+                    <ul>Workouts in this planner: {listWorkouts}</ul>
+                </div>
             </div>
-         </div>
-    )}
+        )
+    }
 }
 
 const mapState = state => {
